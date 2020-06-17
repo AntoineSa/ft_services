55908dfba2634a4f735663b9a1d0e350a9b01fa4
user42 <sablayrolles.antoine@gmail.com>
2020-06-17 17:29:05 +0200

    not working

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,20 +1,7 @@
-sudo usermod -aG docker $USER && newgrp docker;
+#sudo usermod -aG docker $USER && newgrp docker;
 minikube start --driver=docker;
 eval $(minikube docker-env);
 docker build -t nginx ./srcs/nginx ;
 kubectl apply -f srcs/nginx/deployment.yaml;
 kubectl apply -f srcs/nginx/service.yaml;
 kubectl apply -f srcs/ingress/ingress.yaml;
-#export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}');
-#export NODE_PORT=$(kubectl get services/Nginx-node -o go-template='{{(index .spec.ports 0).nodePort}}')
-#kubectl apply -f srcs/wordpress/deployment.yaml;
-kubectl apply -f srcs/mysql/deployment.yaml;
-kubectl apply -f srcs/phpmyadmin/deployment.yaml;
-docker build -t ftps:1.0 ./srcs/ftp ;
-kubectl apply -f srcs/ftp/deployment.yaml;
-kubectl expose deployment/FTPS-node --type="NodePort" --port 21;
-kubectl create deployment ingress_controller-node --image= ;
-kubectl create deployment graphana-node --image= ;
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml;
-kubectl create deployment influxDB-node --image= ;
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml;
