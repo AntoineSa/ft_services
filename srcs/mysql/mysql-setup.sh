0b5f354d9e4c0b3b749f74ac3ac12f264239cf6f
user42 <sablayrolles.antoine@gmail.com>
2020-10-06 16:13:40 +0200

    working inititated db

--- srcs/mysql/mysql-setup.sh
+++ srcs/mysql/mysql-setup.sh
@@ -5,5 +5,12 @@ rm -rf /var/cache/apk/* /etc/my.cnf*
 mysql_install_db -u root
 exec /usr/bin/mysqld -u root --console &
 sleep 5
-mysql -u root -e "CREATE DATABASE $DB_NAME; CREATE USER 'admin'@'%' IDENTIFIED BY 'psswd'; GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION; USE $DB_NAME; FLUSH PRIVILEGES;"
+
+if ! mysql -u root -e 'USE wordpress';
+then
+	mysql -u root -e 'CREATE DATABASE wordpress;'
+	mysql -u root wordpress < wordpress.sql
+fi
+
+mysql -u root -e "USE $DB_NAME; CREATE USER 'admin'@'%' IDENTIFIED BY 'psswd'; GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION; USE $DB_NAME; FLUSH PRIVILEGES;"
 sleep infinite
